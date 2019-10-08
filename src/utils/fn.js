93bd31838188c903d75aa1282c3fe8652540e133
Alex Werner <alex@werner.fr>
2019-10-08 14:14:27 +0200

    fix: allow to correctly get an object

--- src/utils/fn.js
+++ src/utils/fn.js
@@ -1,19 +1,16 @@
 const waitFor = async (watchedObject, watchedProp, successCallback) => {
+  if(watchedObject && watchedObject[watchedProp]) return true;
   return new Promise( (resolve, reject) => {
     let int = null;
     const resolver = () => {
-      console.log('exec')
-
       if (watchedObject[watchedProp]) {
         if(successCallback){
           successCallback();
         }
-        console.log('clear')
         int = clearInterval(int);
         return resolve(true)
       }
     };
-    console.log('set')
     int = setInterval(resolver, 20)
     setTimeout(resolver, 10000);
   });
