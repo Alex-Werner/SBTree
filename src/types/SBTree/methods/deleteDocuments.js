da57da8568b58382f906c09c1d4ae177e4ee9a2c
Alex Werner <alex@werner.fr>
2019-10-08 14:03:29 +0200

    feat: add readiness listener on find and delete

--- src/types/SBTree/methods/deleteDocuments.js
+++ src/types/SBTree/methods/deleteDocuments.js
@@ -1,9 +1,13 @@
 const remove = require('../ops/remove');
+const {waitFor} = require('../../../utils/fn');
 
 async function deleteDocuments(query){
   if(!query || query === {}){
     // this would cause to delete all as we would query all.
     throw new Error('Invalid query')
+  }
+  if(!this.isReady){
+    await waitFor(this, 'isReady');
   }
 
   return (await remove.call(this,query));
