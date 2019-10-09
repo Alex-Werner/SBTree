425272abf972e9e92e2a11c09363c243617da3b5
Alex Werner <alex@werner.fr>
2019-10-09 07:49:55 +0200

    test: added finding doc tests

--- test/e2e/use.case.2.js
+++ test/e2e/use.case.2.js
@@ -1,24 +1,30 @@
 const {expect} = require('chai');
 const {SBTree} = require('../..');
-const {draw}= require('../../src/utils/ascii');
 describe('E2E - Classic UseCase', function suite() {
   describe('RegUser DB', () => {
-    const shiftedUsers = [];
-    const tree = new SBTree();
     const customTree = new SBTree({
       order: 3,
     });
-
-    it('should allow to insert documents with basic nested support', async function () {
-      const doc = {
-        name:"Alex",
-        infos:{
-          job:{
-            sector: "IT"
-          }
+    const alex = {
+      _id: "5d9d74a2668f032206e4dc01",
+      name:"Alex",
+      infos:{
+        job:{
+          sector: "IT"
         }
-      };
-      await customTree.insertDocuments(doc);
+      }
+    };
+    it('should allow to insert documents with basic nested support', async function () {
+      await customTree.insertDocuments(alex);
+    });
+    it('should allow to find the document', async function () {
+      const doc = await customTree.findDocuments({name:"Alex"});
+      expect(doc).to.deep.equal([alex])
+    });
+    it('should not be able to find document on nested',async function () {
+      // May be we should warn user in some way ?
+      const doc = await customTree.findDocuments({infos:{job:{sector:"IT"}}});
+      expect(doc).to.deep.equal([]);
     });
   });
 })
