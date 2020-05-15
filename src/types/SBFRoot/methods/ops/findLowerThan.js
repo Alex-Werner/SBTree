eaf501ca0c72f9721ed7ae9ac3956b9a4e8b43fb
Alex Werner <alex@werner.fr>
2020-05-15 06:58:52 +0200

    style: formatting

--- src/types/SBFRoot/methods/ops/findLowerThan.js
+++ src/types/SBFRoot/methods/ops/findLowerThan.js
@@ -1,6 +1,6 @@
 async function findLowerThan(key, includeKey = false) {
   let result = {identifiers: [], keys: []};
-  const {childrens,identifiers, keys} = this;
+  const { childrens, identifiers, keys } = this;
 
   // We first see where our key is located;
   let leafIndex = 0;
@@ -12,24 +12,24 @@ async function findLowerThan(key, includeKey = false) {
 
   let p = [];
 
-  if(childrens.length===0){
+  if (childrens.length === 0) {
     if(!identifiers[leafIndex]){
-    leafIndex--;
+      leafIndex--;
     }
     if (identifiers[leafIndex]) {
       const last = keys.lastIndexOf(key);
-
-      keys.slice(0, last+1 || leafIndex+1).forEach((_key, i)=>{
-        if(_key<=key){
-          if(_key === key && !includeKey){
+      keys.slice(0, last + 1 || leafIndex + 1).forEach((_key, i) => {
+        if (_key <= key) {
+          if (_key === key && !includeKey) {
             return;
           }
+
           result.identifiers.push(identifiers[i])
           result.keys.push(keys[i])
         }
       })
     }
-  }else{
+  } else {
     // All smaller leaf that our leafIndex needs to be included
     if (leafIndex >= 1) {
       childrens.slice(0, leafIndex).forEach((child) => {
