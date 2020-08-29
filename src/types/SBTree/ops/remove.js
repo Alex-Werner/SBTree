0ab83e81a6c4d87c6ec0d853e48a298efa73920b
Alex Werner <alex@werner.fr>
2020-08-29 04:32:04 +0200

    fix:  handling on unfound field

--- src/types/SBTree/ops/remove.js
+++ src/types/SBTree/ops/remove.js
@@ -26,38 +26,43 @@ async function remove(_query) {
     }
   };
   const results = await query.call(this, _query);
-  for (const result of results) {
-    // TODO : This can be improved.
-    // Indeed, if the loop would happen in the adapter itself
-    // We would need less computation to perform the deletion task.
-    const remCmd = new RemoveCommand(result);
-    for (const _fieldName of remCmd.fields) {
-      const _fieldValue = remCmd.query[_fieldName];
-      const _fieldValType = typeof _fieldValue;
+  if(results !== null){
+    for (const result of results) {
+      // TODO : This can be improved.
+      // Indeed, if the loop would happen in the adapter itself
+      // We would need less computation to perform the deletion task.
+      if(result !== null){
+      const remCmd = new RemoveCommand(result);
+        for (const _fieldName of remCmd.fields) {
+          const _fieldValue = remCmd.query[_fieldName];
+          const _fieldValType = typeof _fieldValue;
 
-      if (['number', 'string', 'boolean'].includes(_fieldValType)) {
-        const fieldNode = this.getFieldTree(_fieldName);
-        await fieldNode.remove(remCmd);
-      } else if (_fieldValType === 'object' && !Array.isArray(_fieldValue)) {
-        for (const _nestedFieldName in _fieldValue) {
-          const _nestedFieldValue = _fieldValue[_nestedFieldName];
-          try {
-            await removeNestedProp(`${_fieldName}.${_nestedFieldName}`, _nestedFieldValue, remCmd);
-          } catch (e) {
-            switch (e.message) {
-              case 'Field do not exist':
-                // Already deletedd probably
-                break;
-              default: throw e;
+          if (['number', 'string', 'boolean'].includes(_fieldValType)) {
+            const fieldNode = this.getFieldTree(_fieldName);
+            await fieldNode.remove(remCmd);
+          } else if (_fieldValType === 'object' && !Array.isArray(_fieldValue)) {
+            for (const _nestedFieldName in _fieldValue) {
+              const _nestedFieldValue = _fieldValue[_nestedFieldName];
+              try {
+                await removeNestedProp(`${_fieldName}.${_nestedFieldName}`, _nestedFieldValue, remCmd);
+              } catch (e) {
+                switch (e.message) {
+                  case 'Field do not exist':
+                    // Already deletedd probably
+                    break;
+                  default: throw e;
+                }
+              }
             }
+          } else {
+            throw new Error(`Unsupported type ${_fieldValType}`);
           }
-        }
-      } else {
-        throw new Error(`Unsupported type ${_fieldValType}`);
-      }
 
-      // Remove documents
-      await (this.getAdapter()).removeDocument(remCmd._id);
+          // Remove documents
+          await (this.getAdapter()).removeDocument(remCmd._id);
+        }
+
+      }
     }
   }
 
