21c5ca2a7f46fe37554bb450382a17ec70578258
Alex Werner <Alex-Werner@users.noreply.github.com>
2023-10-20 14:10:36 +0200

    fix: handle field value as null (#47)

--- src/types/SBTree/utils/getFieldNamesFromQuery.js
+++ src/types/SBTree/utils/getFieldNamesFromQuery.js
@@ -3,7 +3,7 @@ module.exports = function getFieldNamesFromQuery(query) {
   Object.entries(query).forEach((field) => {
     let fieldName = field[0];
     if (fieldName[0] === '$') return;
-
+    if (field[1] === null) return;
     if (field[1].constructor === Object) {
       const _fields = getFieldNamesFromQuery(field[1]);
       if (_fields[0]) {
